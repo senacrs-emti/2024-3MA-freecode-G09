6ee8aaf6206062b22d7953c673eeb15dda38c195
Matheus Cunha <matheusbabinski123@gmail.com>
2024-10-23 11:30:32 -0300

    import html css & js

---
+++ app/src/main/java/com/example/proswim/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.proswim;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
